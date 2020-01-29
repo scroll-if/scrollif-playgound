7573a2bb7bd774239f088299cd6b78c046742bf2
blakedavies <blake@scrollif.com.au>
2020-01-29 10:11:02 +1000

    delete master

--- Gemfile
+++
@@ -1,7 +0,0 @@
-source "https://rubygems.org"
-gem 'github-pages'
-group :jekyll_plugins do
-  gem 'jekyll-menus'
-  gem 'jekyll-minifier'
-  gem 'jekyll-remote-include'
-end
--- _plugins/jekyll-menus.rb
+++
@@ -1,7 +0,0 @@
-# https://github.com/forestryio/jekyll-menus
-
-# Frozen-string-literal: true
-# Copyright: 2015 Forestry.io - MIT License
-# Encoding: utf-8
-
-require "jekyll/menus"
