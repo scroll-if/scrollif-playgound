91abb529b8652e1551b2f0f9518f133d56dd8b81
blakedavies <blake@scrollif.com.au>
2020-03-06 11:24:14 +1000

    update

--- assets/js/concatenated.js
+++ assets/js/concatenated.js
@@ -1,3 +1,4 @@
 ---
 ---
 {% remote_include https://scrollif-assets.netlify.com/websites/js/no-js.js %}
+{% remote_include https://scrollif-assets.netlify.com/websites/components/loading-screen/js/loading-screen.js %}
