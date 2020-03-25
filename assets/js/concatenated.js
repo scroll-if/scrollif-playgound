9321907145e564f647aa6a2f06af75b0c2b269ba
blakedavies <blake@scrollif.com.au>
2020-03-25 18:27:39 +1000

    wipe

--- assets/js/concatenated.js
+++
@@ -1,4 +0,0 @@
----
----
-{% remote_include https://scrollif-assets.netlify.com/websites/js/no-js.js %}
-{% remote_include https://scrollif-assets.netlify.com/websites/components/loading-screen/js/loading-screen.js %}
