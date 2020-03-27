a023db2c4ddc92bf242d86c69899ca3812b402b4
blakedavies <blake@scrollif.com.au>
2020-03-28 01:10:12 +1000

    Revert "Merge branch 'master' into powered-by"
    This reverts commit 13663ffc234dc9b0b507d60248b7361021df4873, reversing
    changes made to 2566dafa4c14fc87413b248cb74d360599c45129.

--- assets/js/concatenated.js
+++ assets/js/concatenated.js
@@ -1,4 +1,3 @@
 ---
 ---
 {% remote_include https://scrollif-assets.netlify.com/websites/js/no-js.js %}
-{% remote_include https://scrollif-assets.netlify.com/websites/components/loading-screen/js/loading-screen.js %}
