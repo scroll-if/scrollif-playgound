15f12978f4c77fc2fdb2c324c16f3776580717aa
Blake (from Scroll If) <blake@scrollif.com.au>
2022-10-09 21:01:47 +1100

    remove test and empty files
    remove header and navbar testing (moved to notion)

--- assets/js/concatenated.js
+++ assets/js/concatenated.js
@@ -2,4 +2,5 @@
 ---
 // GLOBAL JS //
 	{% remote_include https://scrollif-assets.netlify.app/websites/js/no-js.js %}
+	{% remote_include https://scrollif-assets.netlify.app/websites/js/can-touch.js %}
 // EXTERNAL COMPONENTS //
